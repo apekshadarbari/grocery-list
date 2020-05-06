344cbe6e6de4bb424f8f9a9e4b8bb74df1b9495d
Apeksha Darbari <apekshadarbari@gmail.com>
2020-05-05 22:48:48 -0700

    Adding comments and small fixes

--- js/groceryList.js
+++ js/groceryList.js
@@ -1,3 +1,5 @@
+//---------- Declaration of variables to be used in the script  ----------
+
 const tbdStyle = "fa-circle";
 const doneStyle = "fa-check-circle";
 const tbdText = "text";
@@ -6,26 +8,51 @@ const list = document.getElementById("list");
 const inputField = document.getElementById("inputField");
 
 
-function addToList(inputString){
-   const completed = tbdStyle; 
-   const textStyle = tbdText;
-   const item = `<li class=item>
-                <i class="far ${completed}" onclick="markAsDone(this)"></i>
-                <p class="${textStyle}">${inputString}</p>
-                <i class="fas fa-minus-circle" onclick="removeFromList()"></i>
-                </li>
-                `;
-        list.insertAdjacentHTML("beforeend", item);
-        
+//---------- Event Listener to check for 'Enter' key code on a key press and call addToList() if true ----------
+
+document.addEventListener("keyup", function(){
     
-}
+    if(event.keyCode == 13){
+        addToList();
+    } 
+});
+
+
+//---------- Function to add new item to the list ----------
+
+function addToList(){
+   
+
+    
+    if(inputField.value === ''){
+            alert('Please enter the name of the item before adding it.');
+        } 
+        else {
+           
+         
+            const item = `<li class=item>
+                          <i class="far ${tbdStyle}" onclick="toggleClasses(this)"></i>
+                          <p class="${tbdText}">${inputField.value}</p>
+                          <i class="fas fa-minus-circle" onclick="removeFromList()"></i>
+                          </li>`;
+            list.insertAdjacentHTML("beforeend", item);
+            inputField.value="";
+        }
+    
+} 
+
+
+//---------- Function to remove selected item to the list ----------
 
 function removeFromList(){
-    event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+    event.target.parentNode.remove();
 
 }
 
-function markAsDone(element){
+
+//---------- Function to toggle between done and to be done for selected item ----------
+
+function toggleClasses(element){
 
     element.classList.toggle(tbdStyle);
     element.classList.toggle(doneStyle);
@@ -33,17 +60,5 @@ function markAsDone(element){
     element.parentNode.querySelector("p").classList.toggle(doneText);
    
 }
-document.addEventListener("keyup", function(){
-    
-    if(event.keyCode == 13){
-        const inputString = inputField.value;
-        if(inputString === ''){
-            alert('Please enter the name of the item before adding it.');
-        } 
-        else {
-            addToList(inputString);
-            inputField.value="";
-        }
-        
-    }
-});
+
+
